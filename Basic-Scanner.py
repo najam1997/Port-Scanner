082f5eab93c50a9a2bc5d76737417d2e16d508fa
najam1997 <61153309+najam1997@users.noreply.github.com>
2022-10-06 00:10:48 +0500

    Update Basic-Scanner.py

--- Basic-Scanner.py
+++ Basic-Scanner.py
@@ -1,14 +1,22 @@
 import optparse
 from socket import *
+from threading import *
 
+scrn_lock = Semaphore(value=1)
 def Scan(Host_IP, Port):
  try:
   connect_socket = socket(AF_INET, SOCK_STREAM)
   connect_socket.connect((Host_IP, Port))
+  scrn_lock.acquire()
   print ('Port %d is open'% Port)
-  connect_socket.close()
+  
  except:
+  scrn_lock.acquire()
   print ('Port %d is closed'% int(Port))
+ 
+ finally:
+  scrn_lock.release()
+  connect_socket.close()
 
 def portScan(Host, Ports):
  try:
@@ -24,16 +32,19 @@ def portScan(Host, Ports):
  setdefaulttimeout(1)
  
  for Port in Ports:
-  print ("Scanning port " + Port)
-  Scan(Host, int(Port))
+  print(Port)
+  thread1 = Thread(target = Scan, args = (Host, int(Port)))  
+  thread1.start()
+  #print ("Scanning port " + Port)
+  #Scan(Host, int(Port))
 
 def main():
  psr = optparse.OptionParser()
- psr.add_option('-h', dest='Host_IP', type='string')
+ psr.add_option('-u', dest='Host_IP', type='string')
  psr.add_option('-p', dest='Port', type='string')
  (options, args) = psr.parse_args()
  Host_IP = options.Host_IP
- Ports = str(options.Port).split(', ')
+ Ports = str(options.Port).split(',')
  if (Host_IP == None) | (Ports[0] == None):
   print ('No port specified')
   exit(0)
